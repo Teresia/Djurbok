e4dab1a91e08c2d6bb03a22ed9ac843a62e8b3c7
teresia <teresia.schullstrom@gmail.com>
2017-10-11 19:50:50 +0200

    Buggfix och lagt till stänk ikon och refakturerat

--- js/script.js
+++ js/script.js
@@ -1,17 +1,75 @@
-$(".animalCard").click(function(){
+$("#trummor").click(function(){
     document.getElementById("drums").play();
     setTimeout(function(){ 
-     $('#myModal').modal('hide');
+     $('#trummorModal').modal('hide');
      }, 17000);
 });
 
 
-
-
-
-$(".modal").click(function(){
-    document.getElementById("drums").pause();
-    document.getElementById("drums").currentTime = 0;
-    $('#myModal').modal('hide');
+$("#bas").click(function(){
+    document.getElementById("base").play();
+    setTimeout(function(){ 
+     $('#basModal').modal('hide');
+     }, 17000);
 });
+
+
+$("#leadgitarr").click(function(){
+    document.getElementById("leadguitar").play();
+    setTimeout(function(){ 
+     $('#leadgitarrModal').modal('hide');
+     }, 17000);
+});
+
+
+$("#keyboard").click(function(){
+    document.getElementById("rhodes").play();
+    setTimeout(function(){ 
+     $('#keyboardModal').modal('hide');
+     }, 17000);
+});
+
+
+$("#kompgitarr").click(function(){
+    document.getElementById("komp").play();
+    setTimeout(function(){ 
+     $('#kompgitarrModal').modal('hide');
+     }, 17000);
+});
+
+
+$("#song").click(function(){
+    document.getElementById("sing").play();
+    setTimeout(function(){ 
+     $('#songModal').modal('hide');
+     }, 17000);
+});
+
+
+$("#alla").click(function(){
+    document.getElementById("all").play();
+    setTimeout(function(){ 
+     $('#allaModal').modal('hide');
+     }, 17000);
+});
+
+
+$(".closeIcon").click(function(){
+	$('.audio').trigger("pause");
+    document.getElementById("drums").currentTime = 0;
+    document.getElementById("base").currentTime = 0;
+    document.getElementById("leadguitar").currentTime = 0;
+    document.getElementById("rhodes").currentTime = 0;
+    document.getElementById("komp").currentTime = 0;
+    document.getElementById("sing").currentTime = 0;
+    document.getElementById("all").currentTime = 0;
+    $('.modal').modal('hide');
+
+});
+
+
+// $('.modal').modal({
+//   backdrop: 'static',
+//   keyboard: false
+// });
 
